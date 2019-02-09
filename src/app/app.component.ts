d3d0f626baaeda19ce5660fb3b0c5dd9be93b9a9
Alan95 <alan.saber1995@gmail.com>
2019-02-09 17:47:43 +0100

    Create base service/model/component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'spacexlaunches';
+  title:string = 'SpaceX-Launches';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
+import { ListComponent } from './components/list/list.component';
+import { LaunchItemComponent } from './components/launch-item/launch-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListComponent,
+    LaunchItemComponent
   ],
   imports: [
+    NgbModule,
     BrowserModule
   ],
   providers: [],
